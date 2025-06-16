5ed1f2473b69b2c8c04bdf48dc19b8a520e51961
WanchanaJames <janzxcvbf.gun@gmail.com>
2025-06-16 16:40:19 +0700

    argv

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,4 +1,13 @@
-echo "42"
-echo "40"
-echo "2"
-echo "Hello World ! 42"
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied"
+else
+  count=0
+  for arg in "$@"; do
+    if [ $count -lt 3 ]; then
+      echo "$arg"
+      count=$((count + 1))
+    else
+      break
+    fi
+  done
