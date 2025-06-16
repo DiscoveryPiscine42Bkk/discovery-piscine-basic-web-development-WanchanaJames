45826c40e00544e8a81cfb02a778fba2c7e987a8
WanchanaJames <janzxcvbf.gun@gmail.com>
2025-06-16 18:29:49 +0700

    a

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-  echo "No arguments supplied"
-else
-  count=0
-  for arg in "$@"; do
-    if [ $count -lt 3 ]; then
-      echo "$arg"
-      count=$((count + 1))
-    else
-      break
-    fi
-  done
+echo "Argument 1: $1"
+echo "Argument 2: $2"
+echo "Argument 3: $3"
--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+# filepath: build.sh
+for arg in "$@"
+do
+    dir="ex$arg"
+    mkdir -p "$dir"
+    echo "drwxr-xr-x 1  will user40 4, Ok manew 14 22:25 $dir"
+done
