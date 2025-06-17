3b60a3afd75823cfc208df4ded50dc73e4b11fc4
WanchanaJames <janzxcvbf.gun@gmail.com>
2025-06-17 19:55:31 +0700

    balloon

---
+++ cell03/ex3.4/ex01bis/ballon.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    const $balloon = $('#balloon');
+    const $message = $('#message');
+    let size = 200;
+    const colors = ['red', 'green', 'blue'];
+    let colorIndex = 0;
+    let currentColor = colors[colorIndex];
+
+    function updateBalloon() {
+        $balloon.css({
+            'width': size + 'px',
+            'height': size + 'px',
+            'background-color': currentColor
+        });
+    }
+
+    function showExplosion() {
+        $message.css('opacity', 1);
+        $balloon.text("Click me!");
+        
+        setTimeout(() => {
+            $message.css('opacity', 0);
+            updateBalloon();
+        }, 1000);
+    }
+
+    $balloon.on('click', function() {
+        size += 10;
+        
+        if (size > 420) {
+            size = 200;
+            colorIndex = 0;
+            showExplosion();
+        } else {
+            colorIndex = (colorIndex + 1) % colors.length;
+        }
+        
+        currentColor = colors[colorIndex];
+        updateBalloon();
+    });
+
+    $balloon.on('mouseout', function() {
+        size = Math.max(200, size - 5);
+        colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+        currentColor = colors[colorIndex];
+        updateBalloon();
+    });
+});
